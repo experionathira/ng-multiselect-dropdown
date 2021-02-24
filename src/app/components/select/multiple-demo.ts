c3546d382f0a8de3e1318d8d337b31bb1438e183
Athira S <athira.s@experionglobal.local>
2021-02-24 12:33:57 +0530

    Fixes in arabic ui

--- src/app/components/select/multiple-demo.ts
+++ src/app/components/select/multiple-demo.ts
@@ -1,10 +1,10 @@
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
-import { IDropdownSettings } from '../../../ng-multiselect-dropdown/src';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { IDropdownSettings } from "../../../ng-multiselect-dropdown/src";
 
 @Component({
-  selector: 'multiple-demo',
-  templateUrl: './multiple-demo.html'
+  selector: "multiple-demo",
+  templateUrl: "./multiple-demo.html",
 })
 export class MultipleDemoComponent implements OnInit {
   myForm: FormGroup;
@@ -96,71 +96,71 @@ export class MultipleDemoComponent implements OnInit {
 
   ngOnInit() {
     this.cities = [
-      { item_id: 1, item_text: 'New Delhi' },
-      { item_id: 2, item_text: 'Mumbai' },
-      { item_id: 3, item_text: 'Bangalore', isDisabled: this.disableBangalore },
-      { item_id: 4, item_text: 'Pune' },
-      { item_id: 5, item_text: 'Chennai' },
-      { item_id: 6, item_text: 'Navsari' }
+      { item_id: 1, item_text: "New Delhi" },
+      { item_id: 2, item_text: "Mumbai" },
+      { item_id: 3, item_text: "Bangalore", isDisabled: this.disableBangalore },
+      { item_id: 4, item_text: "Pune" },
+      { item_id: 5, item_text: "Chennai" },
+      { item_id: 6, item_text: "Navsari" },
     ];
     this.selectedItems = [
-      { item_id: 4, item_text: 'Pune' },
-      { item_id: 6, item_text: 'Navsari' }
+      { item_id: 4, item_text: "Pune" },
+      { item_id: 6, item_text: "Navsari" },
     ];
     this.dropdownSettings = {
       singleSelection: false,
       defaultOpen: false,
-      idField: 'item_id',
-      textField: 'item_text',
-      selectAllText: 'Select All',
-      unSelectAllText: 'UnSelect All',
+      idField: "item_id",
+      textField: "item_text",
+      selectAllText: "Select All",
+      unSelectAllText: "UnSelect All",
       enableCheckAll: this.showAll,
       itemsShowLimit: 3,
-      allowSearchFilter: this.ShowFilter
+      allowSearchFilter: this.ShowFilter,
     };
     this.myForm = this.fb.group({
-      city: [this.selectedItems]
+      city: [this.selectedItems],
     });
   }
 
   onItemSelect(item: any) {
-    console.log('onItemSelect', item);
-    console.log('form model', this.myForm.get('city').value);
+    console.log("onItemSelect", item);
+    console.log("form model", this.myForm.get("city").value);
   }
   onItemDeSelect(item: any) {
-    console.log('onItem DeSelect', item);
-    console.log('form model', this.myForm.get('city').value);
+    console.log("onItem DeSelect", item);
+    console.log("form model", this.myForm.get("city").value);
   }
 
   onSelectAll(items: any) {
-    console.log('onSelectAll', items);
+    console.log("onSelectAll", items);
   }
 
   onDropDownClose() {
-    console.log('dropdown closed');
+    console.log("dropdown closed");
   }
 
   toogleShowAll() {
     this.showAll = !this.showAll;
     this.dropdownSettings = Object.assign({}, this.dropdownSettings, {
-      enableCheckAll: this.showAll
+      enableCheckAll: this.showAll,
     });
   }
   toogleShowFilter() {
     this.ShowFilter = !this.ShowFilter;
     this.dropdownSettings = Object.assign({}, this.dropdownSettings, {
-      allowSearchFilter: this.ShowFilter
+      allowSearchFilter: this.ShowFilter,
     });
   }
 
   handleLimitSelection() {
     if (this.limitSelection) {
       this.dropdownSettings = Object.assign({}, this.dropdownSettings, {
-        limitSelection: 2
+        limitSelection: 2,
       });
     } else {
       this.dropdownSettings = Object.assign({}, this.dropdownSettings, {
-        limitSelection: -1
+        limitSelection: -1,
       });
     }
   }
@@ -171,6 +171,6 @@ export class MultipleDemoComponent implements OnInit {
   }
 
   handleReset() {
-    this.myForm.get('city').setValue([]);
+    this.myForm.get("city").setValue([]);
   }
 }
